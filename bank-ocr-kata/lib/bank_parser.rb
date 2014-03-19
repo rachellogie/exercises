d528004d122caeb7d13f5bc7ff1dd8ff288cd817
Rachel Logie <rachel.logie1@gmail.com>
2014-03-19 15:56:17 -0600

    BankOCR: Refactor: now takes original graphic instead of array.
    Signed-off-by: Scott Skender <sjskender@gmail.com>

--- bank-ocr-kata/lib/bank_parser.rb
+++ bank-ocr-kata/lib/bank_parser.rb
@@ -1,21 +1,36 @@
 class BankParser
 
   def parse(graphic)
+    graphic_array = parse_graphic(graphic)
     final_number = ""
     i =0
     each_letter = []
 
     9.times do
-      graphic.each do |line|
+      graphic_array.each do |line|
         each_letter << line[i..i+2]
       end
       final_number << translate_to_numeric(each_letter).to_s
       i += 3
-      puts each_letter
       each_letter = []
 
     end
     final_number.to_i
+  end
+
+  private
+
+  def parse_graphic(graphic)
+    array = []
+    graphic.each_line do |line|
+      line = line.chomp
+      if line.length != 18
+        line << "\s"
+      end
+      array << line
+    end
+    array
+
   end
 
   def translate_to_numeric(letter)
--- bank-ocr-kata/spec/bank_parser_spec.rb
+++ bank-ocr-kata/spec/bank_parser_spec.rb
@@ -3,12 +3,16 @@ require 'bank_parser'
 describe BankParser do
   it "returns 123456789 for scanner output" do
     parser = BankParser.new
-    graphic = ["    _  _     _  _  _  _  _ ", "  | _| _||_||_ |_   ||_||_|", "  ||_  _|  | _||_|  ||_| _|"]
+
+    graphic = <<YEEHAW
+    _  _     _  _  _  _  _
+  | _| _||_||_ |_   ||_||_|
+  ||_  _|  | _||_|  ||_| _|
+YEEHAW
 
     actual = 123456789
 
     expected = parser.parse(graphic)
-
 
     expect(actual).to eq expected
   end
